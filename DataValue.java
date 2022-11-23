fc5c7632f3f4e04629cd9c56fc5585703ef54270
Pallavic5 <pchedge235@gmail.com>
2022-11-23 09:49:16 +0530

    Added: Program to display the default value of all primitive data types of Java.

---
+++ DataValue.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.basicprograms;
+
+//1.2 Write a Java program to display the default value of all primitive data types of Java.
+
+public class DataValue {
+
+	public static void main(String[] args) {
+		boolean result = false;
+		char ch = '0';
+		byte a = 0;
+		short b = 0;
+		int c = 0;
+		long d = 0L;
+		float e = 0.0f;
+		double f = 0.0d;
+
+		System.out.println(result);
+		System.out.println(ch);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+
+	}
+
+}
