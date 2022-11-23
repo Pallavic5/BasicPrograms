7fcb90aafce12f676a34a29beeb58f7dc5b9c38d
Pallavic5 <pchedge235@gmail.com>
2022-11-23 09:54:07 +0530

    Added: A Program to get the name using the command line.

---
+++ GetName.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.basicprograms;
+
+//1.5 To get the name using the command line.
+
+public class GetName {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+
+}
